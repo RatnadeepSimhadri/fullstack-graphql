01406581c946cd75806fdfc2ef0730ef762f9e96
Ratnadeep Simhadri <rsimhadri@paypal.com>
2022-11-16 16:31:20 -0800

    Graph QL Intro Schemas, Query and Resolver

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,13 +5,14 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
+    pets(_,{ input }, {models}) {
+      console.log(models)
+      let allpets = models.Pet.findMany()
+      return allpets.filter(pet => pet.type === input.type);
+    }
   },
   Pet: {
-    img(pet) {
+    img(pet,{input}, {models}) {
       return pet.type === 'DOG'
         ? 'https://placedog.net/300/300'
         : 'http://placekitten.com/300/300'
